e4498417b030c2e2704fcffbefc8b0f63d8b3d1e
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 14:55:34 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,9 +6,7 @@ pipeline{
               sh '''
               set +xe
               echo Hello
-              ech Hello
-              sudo yum  install httpd -y
-              ping 8.8.8.8
+              sudo yum  install httpd wget -y
               '''
             }
         }
@@ -16,14 +14,11 @@ pipeline{
             steps{
               ws("/tmp"){
                 sh "pwd"
+                sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                sh "unzip terraform_0.12.7_linux_amd64.zip"
+                sh "mv terraform /bin"
               }
-              // cd /tmp
-              // yum install wget -y
-              // wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
-              // unzip terraform_0.12.7_linux_amd64.zip
-              // mv terraform /bin
-
             }
-            }
-    }
-}
+          }
+        }
+      }
