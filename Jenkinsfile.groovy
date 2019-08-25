7b6170ea31f3c0dcc6cb10b4fc97eae74d4a053b
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 14:43:52 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,9 +4,11 @@ pipeline{
         stage("Run Command"){
             steps{
               sh '''
+              set +xe
               echo Hello
+              ech Hello
               sudo yum  install httpd -y
-              sudo ping 8.8.8.8
+              ping 8.8.8.8
               '''
             }
         }
