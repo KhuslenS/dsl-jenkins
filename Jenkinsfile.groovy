e1c3263dc8eae53729f73f8bca62b990c9247a05
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 15:33:03 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -35,5 +35,26 @@ pipeline{
                 }
             }
         }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip packer_1.4.3_linux_amd64.zip"
+                            sh "rm -rf /sbin/packer"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip"
+                            sh "rm -rf /sbin/packer"
+                            sh "unzip packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
     }
 }
