f6a203ef3c1cde3b8d7649bbbfaf1af30f7a61b1
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 15:47:55 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -62,7 +62,7 @@ pipeline{
         }
         stage("Build Image"){
             steps{
-                sh "packer build image Packer/updated/updated.json"
+                sh "packer build image updated/updated.json"
             }
         }
     }
