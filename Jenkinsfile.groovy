f3fca0f3db0038f03c9083ffc91c26f512c376c3
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 15:13:13 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,6 +20,8 @@ pipeline{
                             sh "sudo mv terraform /bin"
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
                         }
                     }
                 }
