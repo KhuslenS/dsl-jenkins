7923b63602bbbce4a76ac9456fa240b039c3e194
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-28 20:52:31 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -77,6 +77,7 @@ pipeline{
         stage("Terraform "){
             steps{
                 ws("terraform/"){
+                    sh "terraform init"
                     sh "terraform plan -var-file=dev.tfvars"
 
                 }
